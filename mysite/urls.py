da040da20481b6c09a4778c39a260699a2c855e1
Shaurya Prakash Verma <shauryaverma22@gmail.com>
2026-06-15 01:06:15 +0530

    Completed

--- mysite/urls.py
+++ mysite/urls.py
@@ -19,10 +19,13 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from . import views
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", views.home),
+    path("", views.home, name='homepage'),
+    path("employees/", include('employees.urls'))
 ] + static( settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
